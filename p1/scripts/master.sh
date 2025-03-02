0bc05d39096ec27674e525e7aea039c76eb1025a
Abdeljalil-Bouchfar <bouchfar2000@gmail.com>
2025-03-02 14:25:28 +0000

    refactor: simplify node token handling in Vagrantfile and scripts

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -7,9 +7,6 @@ sudo apt-get upgrade -y
 # Install required packages
 sudo apt-get install curl -y
 
-# Set DNS
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
-
 # Install K3s server (master node)
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --flannel-iface=eth1
 
@@ -19,10 +16,4 @@ while ! kubectl get node 2>/dev/null; do
 done
 
 # Get node token for worker nodes
-cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
-
-# Configure kubectl for vagrant user
-mkdir -p /home/vagrant/.kube
-cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-chown -R vagrant:vagrant /home/vagrant/.kube
-echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+cat /var/lib/rancher/k3s/server/node-token > $NODE_TOKEN_FILE
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -9,11 +9,11 @@ sudo apt-get install -y curl
 
 
 # Wait for the node token to be available
-while [ ! -f /vagrant/node-token ]; do
+while [ ! -f $NODE_TOKEN_FILE ]; do
     sleep 1
 done
 
 # Install K3s agent (worker node)
 # Setting the K3S_URL parameter causes the installer to configure K3s as an agent, instead of a server. 
 # The K3s agent will register with the K3s server listening at the supplied URL . 
-curl -sfL https://get.k3s.io | K3S_URL="https://${MASTER_IP}:6443" K3S_TOKEN=$(cat /vagrant/node-token) sh -s -  --flannel-iface=eth1
+curl -sfL https://get.k3s.io | K3S_URL="https://${MASTER_IP}:6443" K3S_TOKEN=$(cat $NODE_TOKEN_FILE) sh -s -  --flannel-iface=eth1
